1ead0165c98e5030e8a05e891d3d74d4807ec90f
Oniichann53 <hoangdt.5.3.2002@gmail.com>
2023-02-10 22:26:36 -0800

    Upload Project 1

---
+++ script.js
@@ -0,0 +1,51 @@
+/* //////////////////////////////////////////////////////////////////////// */
+/* ------------------------ Smooth Scroll Section ------------------------- */
+/* //////////////////////////////////////////////////////////////////////// */
+
+const links = document.querySelectorAll(
+  ".more-btn, .nav-logo, .footer-link, .nav-link"
+);
+links.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+/* ///////////////////////////////////////////////////////////////////// */
+/* ------------------------ Sticky Nav Section ------------------------- */
+/* ///////////////////////////////////////////////////////////////////// */
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
